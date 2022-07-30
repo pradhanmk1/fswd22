0a2d68e526dfeb680d90d31169331f6f8b565dce
Manoj Pradhan <pradhanmk1@gmail.com>
2022-07-29 22:20:09 -0700

    Added Helloworld CRA

---
+++ CRA/helloworldproj/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        
+        Welcome to react training
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
